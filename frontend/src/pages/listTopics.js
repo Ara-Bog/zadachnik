6e9437433a705a989e42882326dfcd15c5ff5f3e
Mikhail <mixicpro@gmail.com>
2024-05-10 15:23:35 +0300

    bug-fix

--- frontend/src/pages/listTopics.js
+++ frontend/src/pages/listTopics.js
@@ -6,23 +6,31 @@ import FetchApi from "../services/fetchApi";
 
 export default function ListTopics({ type }) {
   const [topics, setTopics] = useState(null);
+  const [currentType, setCurrentType] = useState(type);
   const userData = useSelector((state) => state.auth.userData);
   const dispatch = useDispatch();
 
   useEffect(() => {
     FetchApi(`api/topics/${type}`, dispatch, "Ошибка получения списка тем")
       .then((data) => {
-        if (!userData.is_teacher && type !== "project" && data.length > 0) {
-          data.forEach((item) => (item.tags = item.topics_data.reduce((accum, item) => new Set([...accum, ...item.tags]), new Set())));
-        }
-        setTopics(data);
+        setCurrentType(type);
+        setTopics(processData(data, userData));
       })
       .catch(() => {});
   }, [type, userData.name]);
 
+  const processData = (data, userData) => {
+    if (!userData.is_teacher && type !== "project" && data.length > 0) {
+      data.forEach((item) => {
+        item.tags = item.topics_data.reduce((accum, item) => new Set([...accum, ...item.tags]), new Set());
+      });
+    }
+    return data;
+  };
+
   return (
     <div className="list_wrap">
-      {topics && (
+      {topics && currentType === type && (
         <>
           {!topics.length && <h2>На данный момент, в разделе отсутствуют темы</h2>}
           {topics.map((item, indx) =>
